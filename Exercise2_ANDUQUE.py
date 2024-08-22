bc61a37e82347757f86c014467224680bb1a0c15
KurtGabrielAnduque <qkgaanduque@tip.edu.ph>
2024-08-22 12:12:30 +0800

    Update Exercise2_ANDUQUE.py

--- Exercise2_ANDUQUE.py
+++ Exercise2_ANDUQUE.py
@@ -5,9 +5,7 @@ if grade_input >= 75:
     print('passed')    
 elif grade_input == 74:
     print('Remedial')
-elif grade_input < 0:
-    print('dont input negative number')
 elif grade_input < 74 and grade_input >= 0:
     print('Failed')
 else: 
-    print('Invalid input pls input number!!!!')
+    print('dont input negative number')
