617a13605b8c0947ea6ee0de08238fad45f3929a
KurtGabrielAnduque <qkgaanduque@tip.edu.ph>
2024-08-22 12:23:25 +0800

    Update Exercise2_ANDUQUE.py

--- Exercise2_ANDUQUE.py
+++ Exercise2_ANDUQUE.py
@@ -1,11 +1,13 @@
-
 grade_input = int(input("Enter your grade to determine: "))
 
-if grade_input >= 75:
+if grade_input >= 75 and grade_input <= 100:
     print('passed')    
 elif grade_input == 74:
     print('Remedial')
 elif grade_input < 74 and grade_input >= 0:
     print('Failed')
-else: 
-    print('dont input negative number')
+else:
+    print('===============================')
+    print('Grades only range 0-100')
+    print('Dont input negative number')
+    print('===============================')
